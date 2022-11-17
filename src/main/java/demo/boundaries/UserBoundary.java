f81f5e5fa48f18cd8204aa90aa189b82ed0ab240
assaf <assafar26@gmail,com>
2022-11-18 00:23:52 +0200

    Adding GET+POST requests to UserController and building the UserBoundary+NewUserBoundary

--- src/main/java/demo/boundaries/UserBoundary.java
+++ src/main/java/demo/boundaries/UserBoundary.java
@@ -1,5 +1,65 @@
 package demo.boundaries;
 
+import java.util.HashMap;
+
+import demo.enums.Role;
+
 public class UserBoundary {
+	
+	
+	
+	private HashMap<String,String> userId;
+	private Role role;
+	private String username;
+	private String avatar;
+	
+	public UserBoundary() {
+		
+	}
+
+	public UserBoundary(String superApp,String email) {
+		this.userId=new HashMap<>();
+		this.userId.put("superapp", superApp);
+		this.userId.put("email", email);
+		this.role=Role.STUDENT;
+		this.username="Jane Roe";
+		this.avatar="J";
+		
+	}
+
+	public HashMap<String, String> getuserId() {
+		return userId;
+	}
+
+	public void setuserId(HashMap<String, String> userId) {
+		this.userId = userId;
+	}
+
+	public Role getrole() {
+		return role;
+	}
+
+	public void setrole(Role role) {
+		this.role = role;
+	}
+
+	public String getusername() {
+		return username;
+	}
+
+	public void setusername(String username) {
+		this.username = username;
+	}
+
+	public String getavatar() {
+		return avatar;
+	}
+
+	public void setavatar(String avatar) {
+		this.avatar = avatar;
+	}
+	
+	
+	
 
 }
---
+++ src/main/java/demo/controllers/UserController.java
@@ -0,0 +1,38 @@
+package demo.controllers;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.boundaries.NewUserBoundary;
+import demo.boundaries.UserBoundary;
+
+@RestController
+public class UserController {
+	@RequestMapping(
+			path= {"/superapp/users/login/{superapp}/{email}"},
+			method = {RequestMethod.GET},
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+		public UserBoundary loginAndRetreiveDetails(
+				@PathVariable("superapp") String superApp,
+				@PathVariable("email") String email ) {
+		 	
+			return new UserBoundary(superApp,email);
+		}
+
+	@RequestMapping(
+			path= {"/superapp/users"},
+			method = {RequestMethod.POST},
+			produces = {MediaType.APPLICATION_JSON_VALUE},
+			consumes = {MediaType.APPLICATION_JSON_VALUE})
+		public UserBoundary createUser(
+				@RequestBody NewUserBoundary message) {
+		 	String superApp="2023a.demo";
+		 	String email=message.getemail();
+			return new UserBoundary(superApp,email);
+		}
+}
