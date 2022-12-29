73b9eb7aed53e9e8b8ba220fb78a99e727684984
assaf <assaf.ariely@s.ac.il>
2022-12-29 19:46:54 +0200

    handling pagination in ObjectController flow and a few minor corrections

--- src/main/java/superapp/data/interfaces/SuperAppObjectRepository.java
+++ src/main/java/superapp/data/interfaces/SuperAppObjectRepository.java
@@ -3,6 +3,7 @@ package superapp.data.interfaces;
 import java.time.Instant;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import superapp.data.entities.SuperAppObjectEntity;
@@ -13,4 +14,8 @@ public interface SuperAppObjectRepository extends
 
     List<SuperAppObjectEntity> findBycreationTimestampAfter(Instant createdAfter);
 
+	
+
+	List<SuperAppObjectEntity> findAllByParent(SuperAppObjectEntity parent, Pageable page);
+    
 }	
--- src/main/java/superapp/logic/interfaces/ObjectsService.java
+++ src/main/java/superapp/logic/interfaces/ObjectsService.java
@@ -10,6 +10,7 @@ public interface ObjectsService {
 	public ObjectBoundary createObject(ObjectBoundary objWithotId);
 	@Deprecated
 	public ObjectBoundary updateObject(String objectSuperApp,String internalObjectId, ObjectBoundary objectBoundary);
+	
 	public ObjectBoundary getSpecificObject(String objectSuperApp,String internalObjectId);
 	@Deprecated
 	public List<ObjectBoundary> getAllObjects();
